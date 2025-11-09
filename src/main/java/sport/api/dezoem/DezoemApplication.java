cfb397faed6c95af362621278e2176d5a1a63ad3
Deyan Dyankov <deyandyankov23@yahoo.com>
2025-11-09 12:37:11 +0100

    Spring Boot init

---
+++ src/main/java/sport/api/dezoem/DezoemApplication.java
@@ -0,0 +1,13 @@
+package sport.api.dezoem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DezoemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DezoemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sport/api/dezoem/DezoemApplicationTests.java
@@ -0,0 +1,13 @@
+package sport.api.dezoem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DezoemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
